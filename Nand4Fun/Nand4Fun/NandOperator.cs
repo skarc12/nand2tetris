70746d73521c458f7b7fe88fb9a75869cc033f2e
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2013-08-26 09:10:50 -0300

    Nand gate

---
+++ Nand4Fun/Nand4Fun/NandOperator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Nand4Fun
+{
+    [TestFixture]
+    public class NandOperator
+    {
+
+        public bool Nand(bool a, bool b)
+        {
+            return !(a && b);
+        }
+
+        [TestCase(true, true, false)]
+        [TestCase(true, false, true)]
+        [TestCase(false, true, true)]
+        [TestCase(false, false, true)]
+        public void NandTests(bool a, bool b, bool expected)
+        {
+            Nand(a, b).Should().Be(expected);
+        }
+
+
+    }
+}
