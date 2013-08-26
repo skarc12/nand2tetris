53683ab847c850ffc37121673c075cd9b3073e4e
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2013-08-26 15:53:59 -0300

    Nor4fun

---
+++ Nor4fun/Nor4fun/NorOperator.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Nor4fun
+{
+    [TestFixture]
+    public class NorOperator
+    {
+        public bool Nor(bool a, bool b)
+        {
+            return !(a || b);
+        }
+
+        [TestCase(false, false, true)]
+        [TestCase(true, false, false)]
+        [TestCase(false, true, false)]
+        [TestCase(true, true, false)]
+        public void NorTests(bool a, bool b, bool expected)
+        {
+            Nor(a, b).Should().Be(expected);
+        }
+        
+    }
+}
